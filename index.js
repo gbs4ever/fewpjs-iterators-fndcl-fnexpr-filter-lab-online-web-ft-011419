b5ba796061e8ff4084bbee48aed71fbe11f95637
Simcha Greenbaum <gbs4ever2@gmail.com>
2020-01-20 00:09:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 function findMatching(drivers,string) {
 
 let answer = drivers.filter( n => {
-  return n == string
+  return n.upcase() === string.upcase()
 } )
 return answer
 }
  function fuzzyMatch(drviers ,string){
    let answer = drivers.filter( n => {
-     return n[0] == string
+     return n[0] === string
    } )
    return answer
  }
