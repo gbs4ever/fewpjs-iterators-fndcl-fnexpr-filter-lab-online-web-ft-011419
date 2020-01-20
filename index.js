d5c2817e7b14ed5cf7f9a589cb47c355bf89ddc4
Simcha Greenbaum <gbs4ever2@gmail.com>
2020-01-20 00:12:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // Code your solution here
 function findMatching(drivers,string) {
 
-let answer = drivers.filter( n => {
+return drivers.filter( n => {
   return n.toUpperCase() === string.toUpperCase()
 } )
-return answer
+
 }
  function fuzzyMatch(drviers ,string){
-   let answer = drivers.filter( n => {
+   return drivers.filter( n => {
      return n[0] === string
    } )
-   return answer
+
  }
