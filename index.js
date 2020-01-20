ad28b90d815ba527ec50d4af53684519852c6589
Simcha Greenbaum <gbs4ever2@gmail.com>
2020-01-20 00:10:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function findMatching(drivers,string) {
 
 let answer = drivers.filter( n => {
-  return n.toUpperpcase() === string.toUpperpcase()
+  return n.toUppercase() === string.toUppercase()
 } )
 return answer
 }
